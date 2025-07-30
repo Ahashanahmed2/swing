e928beb3ffd1e5c6c55f3d587eb62f7839902893
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2025-07-30 23:48:16 +0800

    Update rsi_diversence.py

--- scripts/rsi_diversence.py
+++ scripts/rsi_diversence.py
@@ -6,8 +6,9 @@ from dotenv import load_dotenv
 
 # 🔐 Load Telegram credentials
 load_dotenv()
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN_TRADE")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID_TRADE")
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
