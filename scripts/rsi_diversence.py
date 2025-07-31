f01097e0fcd170e29be5e3d5aed34d8676b09029
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2025-07-31 19:50:55 +0800

    Update rsi_diversence.py

--- scripts/rsi_diversence.py
+++ scripts/rsi_diversence.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 # 🔐 Load Telegram credentials
 load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID"
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
@@ -30,7 +30,7 @@ def check_rsi_divergence_and_send():
         df['date'] = pd.to_datetime(df['date'])
         df = df.sort_values(by=['symbol', 'date'])
 
-        mongo_df = pd.read_csv('./csv/mongodb.csv')
+        mongo_df = pd.read_csv('/csv/mongodb.csv')
         mongo_df['date'] = pd.to_datetime(mongo_df['date'])
 
         results = []
