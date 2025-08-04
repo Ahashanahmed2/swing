e05b3bf7270e55a0d004c8afff9485bfd1ef95ca
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2025-08-04 21:15:02 +0800

    Update rsi_crose_above_30.py

--- scripts/rsi_crose_above_30.py
+++ scripts/rsi_crose_above_30.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 # Load env variables
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN_TRADE")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID_TRADED")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
