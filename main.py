38009cc005ab5855780e99a390c5247ac77d9408
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2025-11-10 22:29:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ scripts = [
     "scripts/rsi_diver.py",
     #"scripts/confirm_rsi_divergence.py",
     #"scripts/filter_up_candle_to_uptrand.py",
-    "scripts/buy_candle.py",
+    #"scripts/buy_candle.py",
     "scripts/rsi_diver_retest.py",
     'scripts/rsi_crose_above_30.py',
     "scripts/rsi_diversence.py",
