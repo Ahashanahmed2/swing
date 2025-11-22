c3098ddec2a4422a24f11504eb3918a568fc9f7b
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2025-11-22 17:22:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ scripts = [
     #"scripts/buy_candle.py",
     "scripts/rsi_diver_retest.py",
     "scripts/short_buy.py",
+    "scripts/gape.py",
     'scripts/rsi_crose_above_30.py',
     "scripts/rsi_diversence.py",
     'scripts/generate_pdf.py',
