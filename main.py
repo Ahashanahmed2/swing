647397f00d4fa2b3f1a67b8230eae34056c85d04
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2025-12-14 07:44:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 scripts = [
     "scripts/mongodb.py",
     #"scripts/swing_High_Low.py",
-    #"scripts/swing_buy.py",
+    "scripts/swing_buy.py",
     "scripts/liquidly.py",
     "scripts/orderblock.py",
     "scripts/rsi_diver.py",
