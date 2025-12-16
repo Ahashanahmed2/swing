ed267dc582b39a910c2fa50683bd86cdb05302df
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2025-12-16 20:31:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import subprocess
 import os
 
 scripts = [
-    "scripts/mongodb.py",
+    #"scripts/mongodb.py",
     "scripts/swing_High_Low.py",
     "scripts/swing_buy.py",
     "scripts/liquidly.py",
