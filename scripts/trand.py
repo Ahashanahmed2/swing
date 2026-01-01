4e7ea06f5ed60c17c9fcfe5c01be428932dae502
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-01-01 20:23:26 +0800

    Update trand.py

--- scripts/trand.py
+++ scripts/trand.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from datetime import datetime
 
-hf_uploader import download_from_hf_or_run_script
+from hf_uploader import download_from_hf_or_run_script
 
 # Step 1: Download CSV from HF if needed
 if download_from_hf_or_run_script():
