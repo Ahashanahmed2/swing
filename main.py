4043f863b2c1c3d4b6d667018bd5a8a2f472d101
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-02-21 22:08:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ scripts = [
     "scripts/orderblock.py",
     "scripts/rsi_diver.py",
     "scripts/rsi_diver_retest.py",
-    "scripts/rsi_30_buy.csv",
+    "scripts/rsi_crose_above_30.csv",
     "scripts/short_buy.py",
     "scripts/gape.py",
     "scripts/gape_buy.py",
