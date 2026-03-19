e6498c8fda7b500a364875a99dd4500d3c1a229e
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-03-19 12:35:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ scripts = [
     "scripts/sync_emails.py",
     "scripts/trand.py",
     #"scripts/trend_signal.py",
-    "scripts/wave_smc.py",
     "scripts/pattarn.py",
     "scripts/ema_200.py",
     "scripts/support.py",
