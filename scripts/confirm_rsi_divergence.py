118cb4e1708a850986277c9fe76f84d6f4449e80
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-03-29 22:02:33 +0800

    Update confirm_rsi_divergence.py

--- scripts/confirm_rsi_divergence.py
+++ scripts/confirm_rsi_divergence.py
@@ -51,6 +51,6 @@ else:
     os.makedirs('./output/ai_signal', exist_ok=True)
 
     confirmed_df.to_csv('./output/ai_signal/confirm_rsi_diver.csv', index=False)
-    confirmed_df.to_csv('./csv/confirm_rsi_diver.csv', index=False)
+    #confirmed_df.to_csv('./csv/confirm_rsi_diver.csv', index=False)
 
     print("✅ Confirmed RSI divergence saved to both output files.")
