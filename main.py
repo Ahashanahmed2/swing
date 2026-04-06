c469e79ba48a4662130c8e7034adae2b0f034caf
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-04-06 22:24:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ import os
 scripts = [
     
     #"scripts/dse_scraper.py",
-    #"scripts/date_check.py",
-    
+    "scripts/date_check.py",
     "scripts/hf_download.py",
-    
     "scripts/mongodb.py",
     "scripts/support_buy.py",
     "scripts/sync_emails.py",
