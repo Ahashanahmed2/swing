e5328868986d7e87eaba71e6b325f609408fc87b
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-04-08 01:13:19 +0800

    Update llm_train.py

--- scripts/llm_train.py
+++ scripts/llm_train.py
@@ -703,7 +703,7 @@ class AutoLLMTrainer:
         
         # Training config
         if mode == "first_train":
-            num_epochs = 10
+            num_epochs = 3
             learning_rate = 5e-5
             batch_size = 2
         elif mode == "incremental":
