5d71d0c19659633b7d320f7bee9148ddfc245493
Ahashanahmed2 <83818778+Ahashanahmed2@users.noreply.github.com>
2026-04-21 07:10:11 +0800

    Update and rename main_train.py to llm_train.py

---
+++ llm_train.py
@@ -0,0 +1,30 @@
+#github
+#git remote add origin https://github.com/Ahashanahmed2/swing.git
+
+
+
+#main.py
+import sys
+import subprocess
+import os
+
+scripts = [
+    
+    #"scripts/dse_scraper.py",no
+    "scripts/date_check.py",
+    "scripts/hf_download.py",
+    "scripts/generate_pattern_training_data_complete.py",
+    "scripts/llm_train.py",
+]
+
+for script in scripts:
+    script_path=os.path.abspath(script)
+    try:
+        subprocess.run([sys.executable,script_path],check=True)
+        print(f"Finished {script}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {script}:{e}")
+        break
+    except FileNotFoundError:
+        print(f"File not found :{script}")
+        break
